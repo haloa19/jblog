62b6952e935c39cfb11041804da8c1f741c8c50d
haloa19 <hj199514@gmail.com>
2020-04-21 19:34:53 +0900

    jblog05 ajax버젼

--- jblog05/src/main/java/com/douzone/jblog/config/web/MvcConfig.java
+++ jblog05/src/main/java/com/douzone/jblog/config/web/MvcConfig.java
@@ -1,7 +1,17 @@
 package com.douzone.jblog.config.web;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
